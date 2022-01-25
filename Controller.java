fbcf7bdc33f5ac27db2ed2a687f58aa1efffefd4
Murad Nabizade <66982842+MuradNabizade@users.noreply.github.com>
2022-01-24 18:58:00 -0600

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,42 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.stage.Stage;
+
+import javafx.event.ActionEvent;
+import java.io.IOException;
+
+public class Controller {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    private ComboBox<?> hoursInput;
+
+    @FXML
+    private ComboBox<?> minuteInput;
+
+    @FXML
+    private Button onStart;
+
+    @FXML
+    private ComboBox<?> secondInput;
+
+    @FXML
+    void OnStart(ActionEvent event){
+
+
+
+    }
+
+
+
+
+
+}
